12eb96165cf6bc9755a50a4bbd236165ed3aae2a
Mark Peters <mapeters101@earthlink.net>
2025-02-13 21:55:29 -0500

    Work on lesson006.

---
+++ lesson006.py
@@ -0,0 +1,18 @@
+if __name__ == '__main__':
+
+    my_list = [1, 2, 3, 4]
+    print(my_list)
+    print("-"*100)
+
+    my_list.append(5)
+    print(my_list)
+    print("-"*100)
+
+    print(my_list[2])
+    print("-"*100)
+
+    added_list = my_list + [6, 7, 8]
+    print(added_list)
+    print("-" * 100)
+
+
