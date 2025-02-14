f41c36c0de6fc68b1baee09cb154fb661c277508
Mark Peters <mapeters101@earthlink.net>
2025-02-13 22:01:40 -0500

    Work on lesson008 to 2:00.

---
+++ lesson008.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    my_dictionary = {"name": "John",
+                     "age": 25}
+
+    my_dictionary = dict (name="John",
+                          age=25)
+
+    my_dictionary["age"] = 26
+    print(my_dictionary['age'])
+
+
