fa350c61024988504be8f9bb759c720e50a57682
Mark Peters <mapeters101@earthlink.net>
2025-02-13 22:05:07 -0500

    Complete work on lesson008 from 2:00.

--- lesson008.py
+++ lesson008.py
@@ -8,4 +8,9 @@ if __name__ == '__main__':
     my_dictionary["age"] = 26
     print(my_dictionary['age'])
 
+    students = [
+        {"name": "John", "grades": (90, 85, 88)},
+        {"name": "Jane", "grades": [90, 85, 88]}
+    ]
+    print(students[0]['grades'][1])
 
